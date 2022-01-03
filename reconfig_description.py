a082ee4c7d9f2c7cd0cc2344ce9af7fda9324514
rishrapsody <rishrapsody@gmail.com>
2022-01-03 16:07:28 +0530

    added 2nd logic to script

--- reconfig_description.py
+++ reconfig_description.py
@@ -9,10 +9,21 @@ nr = InitNornir(config_file="config.yml")
 def pull_cdp(task):
     output = task.run(task=send_command, command="show cdp neighbors")
     task.host["facts"] = output.scrapli_response.genie_parse_output()
-    print(task.host)
+#    print(task.host)
     pprint(task.host["facts"])
+    inner_list = []
+    outer_list = []
+    test_dict = {}
+    for key,value in task.host["facts"]["cdp"]["index"].items():
+        inner_list.append(key["device_id"])
+        inner_list.append(key["port_id"])
+        test_dict[key["local_interface"]] = outer_list.append(inner_list)
+    pprint(test_dict)
+
+
+
 
 
 results = nr.run(task=pull_cdp)
 #print_result(results)
-ipdb.set_trace()
+#ipdb.set_trace()
