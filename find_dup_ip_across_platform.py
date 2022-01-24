5fc72be42a7c58acf754cf40fd001839fffc931a
rishrapsody <rishrapsody@gmail.com>
2022-01-24 22:16:21 +0530

    added code to identity device with dup ip

--- find_dup_ip_across_platform.py
+++ find_dup_ip_across_platform.py
@@ -8,6 +8,62 @@ from collections import Counter
 
 nr = InitNornir(config_file="config.yml")
 global_list = []
+global_dev_list = []
+global outer_dict
+outer_dict = { }
+global outer_dict2
+outer_dict2 = { }
+global dup_ip
+
+def get_dup_devices(task):
+    if task.host.platform == 'ios' or task.host.platform == 'iosxr':
+        cisco_list = get_cisco_devices(task)
+#        print(cisco_list)
+        global_dev_list.extend(cisco_list)
+    elif task.host.platform == 'nxos':
+        nxos_list = get_nxos_devices(task)
+        global_dev_list.extend(nxos_list)
+    else:
+        junos_list = get_junos_devices(task)
+        global_dev_list.extend(junos_list)
+
+def get_cisco_devices(task):
+    output = task.run(task=send_command, command="show ip int brief", severity_level=logging.DEBUG)
+    task.host["facts_new"] = output.scrapli_response.genie_parse_output()
+    inner_dict = { }
+    inner_list = []
+    for key, value in task.host["facts_new"]["interface"].items():
+        if value["ip_address"] != "unassigned":
+            inner_dict = { }
+ #           inner_list = []
+            if value["ip_address"] in dup_ip:
+                inner_dict[task.host] = key
+                inner_list.append(inner_dict)
+                outer_dict[value["ip_address"]] = inner_list
+                inner_dict = { }
+            else:
+                pass
+        else:
+            pass
+
+def get_nxos_devices(task):
+    output = task.run(task=send_command, command="show interface", severity_level=logging.DEBUG)
+    task.host["facts_new"] = output.scrapli_response.genie_parse_output()
+    inner_dict2 = {}
+    inner_list2 = []
+    for key, value in task.host["facts"].items():
+        if "ipv4" in value:
+            for i,j in value["ipv4"].items():
+                if j["ip"] in dup_ip:
+                    inner_dict2[task.host] = key
+                    inner_list2.append(inner_dict2)
+                    outer_dict2[j["ip"]] = inner_list2
+                    inner_dict2 = {}
+        else:
+            pass
+
+
+
 
 def get_ip(task):
     if task.host.platform == 'ios':
@@ -21,9 +77,6 @@ def get_ip(task):
         global_list.extend(nxos_list)
     else:
         find_ip_junos(task)
-
-
-
 
 def get_ip_ios(task):
     output = task.run(task=send_command, command="show ip int brief", severity_level=logging.DEBUG)
@@ -55,15 +108,21 @@ def get_ip_iosxr(task):
     for key,value in task.host["facts"]["interface"].items():
         if value["ip_address"] != "unassigned":
             sub_list.append(value["ip_address"])
+        else:
+            pass
     print("sub list for {} is {}".format(task.host, sub_list))
     return(sub_list)
 
 devices = nr.filter(F(groups__contains='cloud'))
 results = devices.run(task=get_ip)
-#results = nr.run(task=get_net)
+#results = nr.run(task=get_ip)
 
 dup_ip = [item for item, count in Counter(global_list).items() if count > 1]
 if dup_ip:
     print("Duplicate IP's found: {}".format(dup_ip))
+    temp = nr.run(task=get_dup_devices)
+    print(outer_dict)
+    print(outer_dict2)
+
 else:
     print("No Duplicate IPs found")
