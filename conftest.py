b72edcbc91c8038a67e94ad2bbf31e93bc532092
rishrapsody <rishrapsody@gmail.com>
2022-01-31 15:33:38 +0530

    updated script

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from nornir import InitNornir
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def nr():
     nr = InitNornir(config_file="config.yml")
     yield nr
