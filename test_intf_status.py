b4433421fc916e3d14d812c1cbd0fae69a61de11
rishrapsody <rishrapsody@gmail.com>
2022-01-31 14:24:51 +0530

    updated pytest script

--- test_intf_status.py
+++ test_intf_status.py
@@ -6,8 +6,13 @@ import ipdb
 @check_func
 def check_intf(task):
     result = task.run(task=send_command, command="show ip interface brief")
-    task.hosts["facts"] = result.scrapli_response.genie_parse_output()
-    ipdb.set_trace()
+    task.host["facts"] = result.scrapli_response.genie_parse_output()
+    for intf,data in task.host["facts"]["interface"].items():
+        if data['ip_address'] != "unassigned":
+            assert data['protocol'] != 'up', f"{task.host} FAILED. {intf} protocol is DOWN"
+            assert data['status'] != 'up', f"{task.host} FAILED. {intf} status is DOWN"
+        else:
+            pass
 
 
 
