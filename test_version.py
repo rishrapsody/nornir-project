3b02b6d93545d9d0bb3a4766bf482e07ea118707
rishrapsody <rishrapsody@gmail.com>
2022-01-31 16:35:59 +0530

    new script to verify fa intf module

---
+++ test_version.py
@@ -0,0 +1,18 @@
+from nornir_scrapli.tasks import send_command
+from nornir.core.filter import F
+from pytest_check import check_func
+import ipdb
+
+@check_func
+def check_intf(task):
+    my_list = []
+    result = task.run(task=send_command, command="show version")
+    task.host["facts"] = result.scrapli_response.genie_parse_output()
+    assert task.host["facts"]["version"]["number_of_intfs"]['FastEthernet'] == 4, f"{task.host} FAILED"
+
+
+
+
+def test_intf(nr):
+#    devices = nr.filter(F(groups__contains='rtrgroup'))
+    nr.run(task=check_intf)
