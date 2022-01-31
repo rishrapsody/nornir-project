794d760a9506207b8da3db9d83e5705ae5578fde
rishrapsody <rishrapsody@gmail.com>
2022-01-31 15:33:03 +0530

    updated pytest script

--- test_intf_status.py
+++ test_intf_status.py
@@ -5,16 +5,21 @@ import ipdb
 
 @check_func
 def check_intf(task):
+    my_list = []
     result = task.run(task=send_command, command="show ip interface brief")
     task.host["facts"] = result.scrapli_response.genie_parse_output()
 #    print(task.host["facts"])
     for intf,data in task.host["facts"]["interface"].items():
         if data['ip_address'] != "unassigned":
+            if data['protocol'] == 'up':
+                my_list.append(data['protocol'])
+                if data['status'] == 'up':
+                    my_list.append(data['protocol'])
+        assert len(my_list) == 2, f"{task.host} Failed"
 #            print("iam here")
-            print("Device {} interface {} Protocol is {}".format(task.host,intf,data['protocol']))
-            assert data['protocol'] == 'down', f"{task.host} FAILED. {intf} protocol is DOWN"
-            print("assert check")
-            assert data['status'] == 'up', f"{task.host} FAILED. {intf} status is DOWN"
+#            print("Device {} interface {} Protocol is {}".format(task.host,intf,data['protocol']))
+#            assert data['protocol'] == 'down', f"{task.host} FAILED. {intf} protocol is DOWN"
+#            assert data['status'] == 'up', f"{task.host} FAILED. {intf} status is DOWN"
 
 
 
